af3f39e2fe18568dfe58f3ada3cdd280a32e9f17
kts123 <taeksoon@gmail.com>
2014-11-23 04:09:52 -0800

    srm634div2lev1

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,14 @@
 -- | Main entry point to the application.
 module Main where
 
-countPeaks  []  = 0
-countPeaks  xs  = length [b |(a,b,c)<-zips, a < b && b > c]
-        where
-            lb             = minimum xs - 1            
-            zips           = zip3 (lb:xs) xs (tail xs ++ [lb])
-                
+-------------------------------------------------------
+countPeaks [] = 0
+countPeaks xs = length [x| let g  = minimum xs - 1,
+                           let ls = g:xs,
+                           let rs = tail xs ++ [g],
+                           (l,x,r) <-zip3 ls xs rs,
+                             l < x && x > r]
+-------------------------------------------------------
 -- | The main entry point.
 main :: IO ()
 main = do
@@ -16,3 +18,4 @@ main = do
     print $ countPeaks [2,5,3,7,2,8,1,3,1]
     print $ countPeaks [1]
     print $ countPeaks [1,2,3,4,4,3,2,1]
+    print $ countPeaks []
