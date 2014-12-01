ba8d1dc455eb0a1b8d73d7e295b9fc8353abc22c
kts123 <taeksoon@gmail.com>
2014-12-01 10:43:01 -0800

    리커전 버전을 foldl 버전으로 변경

--- src/Srm633Div2Lev1.hs
+++ src/Srm633Div2Lev1.hs
@@ -6,18 +6,19 @@ module Srm633Div2Lev1 where
 gen c n =  "#" ++ take (n-2) (repeat c) ++ "#"
 
 ------------------------------------------------------------------
-target 1 =    ["#"]
+accOp acc n =    [gen '#' n               ] -- ["#########"]
+              ++ [gen ' ' n               ] -- ["#       #"]
+              ++ ["# "++ x ++" #" | x<-acc] -- ["# .acc. #"]
+              ++ [gen ' ' n               ] -- ["#       #"]
+              ++ [gen '#' n               ] -- ["#########"]
 
-target n =    [gen '#' n                        ] -- ["#########"]
-           ++ [gen ' ' n                        ] -- ["#       #"]
-           ++ ["# "++ x ++" #" | x<-target (n-4)] -- ["# ..... #"]
-           ++ [gen ' ' n                        ] -- ["#       #"]
-           ++ [gen '#' n                        ] -- ["#########"]
+target n =  foldl accOp ["#"] [x | x<-[1..n], mod x 4 == 1,  1 < x]
 
 ------------------------------------------------------------------
 -- | The main entry point.
 main :: IO ()
 main = do
+    mapM_ putStrLn $ ["n = 1"]  ++ target 1  ++ ["\n"]
     mapM_ putStrLn $ ["n = 5"]  ++ target 5  ++ ["\n"]
     mapM_ putStrLn $ ["n = 9"]  ++ target 9  ++ ["\n"]
     mapM_ putStrLn $ ["n = 13"] ++ target 13 ++ ["\n"]
