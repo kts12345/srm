cc785acffadd9dabd41adfd60605aa547173eea8
kts123 <taeksoon@gmail.com>
2014-12-03 11:07:28 -0800

    using boost algorithm

--- src/Srm633Div2Lev1.cpp
+++ src/Srm633Div2Lev1.cpp
@@ -1,68 +1,66 @@
 // http://community.topcoder.com/stat?c=problem_statement&pm=13462
-
 #include <vector>
 #include <iostream>
-#include <numeric>
+#include <boost/range/numeric.hpp>
+#include <boost/range/algorithm.hpp>
+#include <boost/range/irange.hpp>
 
 //-----------------------------------------------------------
-// typedef && Util
+// typedef & Util
 
 using list_t = std::vector<std::string>;
 
-list_t operator+(list_t&& lhs, const list_t& rhs)
+list_t operator+(list_t lhs, const list_t& rhs)
 { 
     lhs.insert(lhs.end(), rhs.begin(), rhs.end());
     return lhs;
 }
 
-auto map = [](auto&& list, auto fn)
+auto map = [](auto xs, auto fn)
 {
-    std::transform(list.begin(), list.end(), list.begin(), 
-                   [=](auto& x) { return fn(x); });
-    return list;
-};
-
-auto print = [](auto&& table)
-{ 
-    for(auto& e : table) 
-        std::cout << e << std::endl; 
+    boost::transform(xs, xs.begin(), [=](auto& x) { return fn(x); });
+    return xs;
 };
 
 //===========================================================
 // Srm633Div2Lev1
 //-----------------------------------------------------------
-auto target(std::size_t n)
+auto acc_op = [](auto&& acc, int x)
 {
-    auto accOp = [](auto&& acc, std::size_t n)
-    {
-        auto gen= [](char c, auto n) { return "#" + std::string(n-2, c) +"#"; };
-        
-        return 
-          list_t{ gen('#', n) }    // -- ["###############"]
-        + list_t{ gen(' ', n) }    // -- ["#             #"]
-        + map(acc, [](auto& row) { return "# " + row + " #"; })
-        + list_t{ gen(' ', n) }    // -- ["#             #"]
-        + list_t{ gen('#', n) };   // -- ["###############"]
-    };
-
-    // {5, 9, ..., n/4}
-    std::vector<std::size_t> l(n/4);
-    std::iota(l.begin(), l.end(), 1);
-    std::transform(l.begin(), l.end(), l.begin(), [](int n) { return 4*n+1;});
+    auto fill = [=](char e) { return std::string(x-4, e); };
+    return 
+      list_t{"##" + fill('#') + "##"}  //["###########"]
+    + list_t{"# " + fill(' ') + " #"}  //["#         #"]
+    + map(acc, [](auto& a){ return        "# "+ a +" #";}) 
+    + list_t{"# " + fill(' ') + " #"}  //["#         #"]
+    + list_t{"##" + fill('#') + "##"}; //["###########"]
+};
     
-    return std::accumulate(l.begin(), l.end(), list_t{"#"}, accOp);
+auto target(int n)
+{
+    using start_t = int;    
+    using last_t  = int;
+    using step_t  = int;
+    using init_t  = list_t;
+    
+    auto xs = boost::irange(start_t{5}, last_t{n<5?5:n+1}, step_t{4}); // [5,9,..,n]
+    return boost::accumulate(xs, init_t{"#"}, acc_op);
 }
 
 //-----------------------------------------------------------
 int main()
 {
+    auto print = [](const auto& table){ 
+        for(const auto& e : table) 
+            std::cout << e << std::endl; 
+    };
+
     print (list_t{"n=1" } + target (1)  + list_t{"\n"});
     print (list_t{"n=5" } + target (5)  + list_t{"\n"});
     print (list_t{"n=9" } + target (9)  + list_t{"\n"});
     print (list_t{"n=13"} + target (13) + list_t{"\n"});
     print (list_t{"n=17"} + target (17) + list_t{"\n"});
 }
-
 //clang++  -std=c++1y  -ldl -lcxxrt --stdlib=libc++  main.cpp && ./a.out
 
 /*
