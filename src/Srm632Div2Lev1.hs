bbb98268a2019bf037de0668eb918dc983887932
kts123 <taeksoon@gmail.com>
2014-12-07 14:30:02 -0800

    Srm632Div2Lev1

--- src/Srm632Div2Lev1.hs
+++ src/Srm632Div2Lev1.hs
@@ -1,1 +1,12 @@
 module Srm632Div2Lev1 where
+
+handler (prev, lap) x = (x, lap + if prev < x then 0 else 1)
+numberOfLap n xs = snd $ foldl handler (n,0) xs
+
+-- | The main entry point.
+main :: IO ()
+main = do
+        print $ numberOfLap 3  [1,2,3]
+        print $ numberOfLap 24 [6,6]
+        print $ numberOfLap 3  [3,2,1]
+        print $ numberOfLap 50 [1,3,5,7,9,2,4,6,8,10]
