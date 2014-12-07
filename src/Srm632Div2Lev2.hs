8b74d35730dd4956fa92a0a73d4b586ea60ee582
kts123 <taeksoon@gmail.com>
2014-12-07 15:33:46 -0800

    temp

--- src/Srm632Div2Lev2.hs
+++ src/Srm632Div2Lev2.hs
@@ -3,7 +3,19 @@
 module Srm632Div2Lev2 where
  
 --numberOfSubsequences xs = repeat 2 3
+handler (diff,count) (prev, x) = (x - prev, if -prev == diff then count+3 else count + 2)
+toDiff (prev,x) = x-prev
+toCount (prev, x) = if prev == x then 3 else 2
+numberOfSubsequences xs = sum counts
+    where
+       counts = map toCount zip(diffs)
 
+     --  {1 ,    2,     4,     8,     16 }
+     --  {   (1,2), (2,4), (4,8), (8,16) } <= zip
+     --  {       1,     2,     4,      8 } <= toDiff
+     --  {          (1,2),  (2,4), (4,8) } <= zip
+     --  { #1   #2,     2       2      2 } <= toCount
+     
 -- | The main entry point.
 main :: IO ()
 main = do
