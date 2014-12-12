ff0ac3cc0d29dadb2f7651f5c01b6460d3edcc4f
kts123 <taeksoon@gmail.com>
2014-12-12 09:21:05 -0800

    Srm632Div2Lev1

--- src/Srm631Div2Lev1.hs
+++ src/Srm631Div2Lev1.hs
@@ -1,23 +1,29 @@
+-- Srm632Div2Lev1 TaroGrid
+-- http://community.topcoder.com/stat?c=problem_statement&pm=13394
+
 module Srm631Div2Lev1 where
 
 ------------------------------------------------------
-handler (counts, maxs) (prevs, nows) = (counts',maxs')
-  where counts' = zipWith3 (\c p n -> if p ==n then c+1 else 1) counts prevs nows
-        maxs'   = zipWith max maxs counts'
+handler (cnts,maxs) (prvs,nows) = (cnts', zipWith max cnts' maxs)
+   where cnts' = zipWith3 (\p n c-> if p==n then c+1 else 1) prvs nows cnts
 ------------------------------------------------------
-taroGrid (x:xs) =
-    maximum $ snd $ foldl handler init $ zip (x:x:xs) (x:xs) 
-     -- where init = ((replicate (length x) 1), (replicate (length x) 0), x)
-          where init = ([0,0..], [0,0..])
+taroGrid (x:xs) = maximum $ snd $ foldl handler ([0,0..],[0,0..]) $ zip (x:x:xs) (x:xs)
 ------------------------------------------------------
 -- | The main entry point.
 main :: IO ()
 main = do
-  --   print $ (\(_,s,_)-> s) ([1,1..], [0,0..], "B")
-  --  print $ taroGrid ["W"]  
-    print $ take 4 "UU.."
+    print $ taroGrid ["W"]  
     print $ taroGrid ["WB", "BW"]    
     print $ taroGrid ["BWW", "BBB", "BWB"]    
     print $ taroGrid ["BWBW", "BBWB", "WWWB", "BWWW"]
     print $ taroGrid ["BWB", "BBW", "BWB"] 
     print $ taroGrid ["BBWWBBWW", "BBWWBBWW", "WWBBWWBB", "WWBBWWBB", "BBWWBBWW", "BBWWBBWW", "WWBBWWBB", "WWBBWWBB"]
+
+{-| Output
+1
+1
+3
+3
+3
+2
+-}
