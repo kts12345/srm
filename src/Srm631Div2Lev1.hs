8372222ca130cf40a6a52d354a753451000710d2
kts123 <taeksoon@gmail.com>
2014-12-12 09:22:24 -0800

    max 처리 버그 수정

--- src/Srm631Div2Lev1.hs
+++ src/Srm631Div2Lev1.hs
@@ -1,16 +1,12 @@
 -- Srm632Div2Lev1 TaroGrid
 -- http://community.topcoder.com/stat?c=problem_statement&pm=13394
-
 module Srm631Div2Lev1 where
-
 ------------------------------------------------------
 handler (cnts,maxs) (prvs,nows) = (cnts', zipWith max cnts' maxs)
    where cnts' = zipWith3 (\p n c-> if p==n then c+1 else 1) prvs nows cnts
 ------------------------------------------------------
 taroGrid (x:xs) = maximum $ snd $ foldl handler ([0,0..],[0,0..]) $ zip (x:x:xs) (x:xs)
 ------------------------------------------------------
--- | The main entry point.
-main :: IO ()
 main = do
     print $ taroGrid ["W"]  
     print $ taroGrid ["WB", "BW"]    
