b3edf56bc14c5692c6651162aac89f3c41e3b5b0
kts123 <taeksoon@gmail.com>
2014-12-12 07:50:17 -0800

    temp

--- src/Srm631Div2Lev1.hs
+++ src/Srm631Div2Lev1.hs
@@ -5,8 +5,8 @@ handler (counts, maxs) (prevs, nows) = (counts',maxs')
   where counts' = zipWith3 (\c p n -> if p ==n then c+1 else 1) counts prevs nows
         maxs'   = zipWith max maxs counts'
 ------------------------------------------------------
-taroGrid xs =
-    maximum $ snd $ foldl handler init $ zip (['U','U'..]:xs) xs 
+taroGrid (x:xs) =
+    maximum $ snd $ foldl handler init $ zip (x:x:xs) (x:xs) 
      -- where init = ((replicate (length x) 1), (replicate (length x) 0), x)
           where init = ([0,0..], [0,0..])
 ------------------------------------------------------
@@ -14,7 +14,8 @@ taroGrid xs =
 main :: IO ()
 main = do
   --   print $ (\(_,s,_)-> s) ([1,1..], [0,0..], "B")
-  --  print $ taroGrid ["W"]    
+  --  print $ taroGrid ["W"]  
+    print $ take 4 "UU.."
     print $ taroGrid ["WB", "BW"]    
     print $ taroGrid ["BWW", "BBB", "BWB"]    
     print $ taroGrid ["BWBW", "BBWB", "WWWB", "BWWW"]
