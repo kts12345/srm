087f6dcdea1bb690b077cd69e978e587af978634
kts123 <taeksoon@gmail.com>
2014-12-12 10:31:12 -0800

    Srm631Div2Lev2

--- src/Srm631Div2Lev2.hs
+++ src/Srm631Div2Lev2.hs
@@ -1,35 +1,28 @@
 module Srm631Div2Lev2 where
-    
+import Data.Maybe
 import Data.List
 import Control.Monad
-
 ------------------------------------------------------
-handlerS (time, start) (position, count) = timeStart 
-    where start'    = max start (position - time)
-          end       = start' + count - 1
-          endLimit  = position + time
-          timeStart = if end <= endLimit then Just (time, end+1)
-                                         else Nothing 
-
+cJust v cond = if cond then Just v else Nothing
 ------------------------------------------------------
-handler (time,xs) (p,c) = next 
-        where xs'   = sort $ xs ++ [(p,c)] 
-              check = foldM handlerS (time, -2001) xs'
-              next  = if check == Nothing then Nothing
-                                          else Just (time, xs') 
-
+handlerS (time, start) (pos, cnt) = cJust (time,end'+1) possible
+  where  (low,   high) = (pos-time, pos+time)
+         (start',end') = (max start low, start' + cnt-1)  
+         possible      = end' <= high
+------------------------------------- -----------------
+handler (time,xs) (p,c) = cJust (time,xs ++ [(p,c)])   $
+                          isJust                       $
+                          foldM handlerS (time, -2001) $ 
+                          sort                         $ 
+                          xs ++ [(p,c)] 
 ------------------------------------------------------
-catsOnTheLine ps cs time = 
-    if result == Nothing then "Impossible" else "Possible"
-    where result = foldM handler (time, []) $ zip ps cs
-
+catsOnTheLine ps cs time = if possible then "Possible" else "ImPossible"
+  where possible = isJust $ foldM handler (time, []) $ zip ps cs
 ------------------------------------------------------
--- | The main entry point.
-main :: IO ()
 main = do
         print $ catsOnTheLine [0] [7] 3
         print $ catsOnTheLine [0] [8] 2
         print $ catsOnTheLine [0,1] [3,1] 0
         print $ catsOnTheLine [5, 0, 2] [2, 3, 5] 2
-        print $ catsOnTheLine [5, 1, -10, 7, 12, 2, 10, 20] [3, 4,   2, 7,  1, 4,  3,  4] 6
+        print $ catsOnTheLine [5, 1, -10, 7, 12, 2, 10, 20] [3, 4, 2, 7, 1, 4, 3, 4] 6
 
