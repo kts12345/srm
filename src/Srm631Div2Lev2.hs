57e3244ac129eee47c90727a2af36748c7d30720
kts123 <taeksoon@gmail.com>
2014-12-14 11:22:42 -0800

    sort 제거

--- src/Srm631Div2Lev2.hs
+++ src/Srm631Div2Lev2.hs
@@ -16,7 +16,7 @@ handler' events evt  = cJust isPossible events'
           isPossible = isJust $ foldM_ handler [-2001] events'
 ------------------------------------------------------
 catsOnTheLine ps cs time = toString $ foldM_ handler' [] events
-    where events     = sort [(p-time, p+time, cnt)|(p,cnt)<-zip ps cs]
+    where events     =  [(p-time, p+time, cnt)|(p,cnt)<-zip ps cs]
           toString v = if isJust v then "Possible" else "Impossible"
 ------------------------------------------------------
 main = do
