9e549c266065426a922f9700f72dfbf72fca8de8
kts123 <taeksoon@gmail.com>
2014-12-16 05:36:40 -0800

    rename : h->top

--- src/Srm630Div2Lev1.hs
+++ src/Srm630Div2Lev1.hs
@@ -2,10 +2,10 @@
 -- http://community.topcoder.com/stat?c=problem_statement&pm=13378&rd=16061
 module Srm630Div2Lev1 where
 ------------------------------------------------------
-handler []        evt  = [evt]         -- push
-handler (h:stack) evt
-           | h == evt  = (stack)       -- pop
-           | otherwise = (evt:h:stack) -- push
+handler []          evt = [evt]         -- push
+handler (top:stack) evt
+           | top == evt = (stack)       -- pop
+           | otherwise  = (evt:h:stack) -- push
 ------------------------------------------------------
 doubleLetter xs = if [] == foldl handler [] xs then "Possible" else "Impossible"
 ------------------------------------------------------
