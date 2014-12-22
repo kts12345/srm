75116acbac7c3716c8bb89837a257adc8b7e727e
kts123 <taeksoon@gmail.com>
2014-12-22 15:17:05 -0800

    temp. bug.

--- src/Srm630Div2Lev2.hs
+++ src/Srm630Div2Lev2.hs
@@ -4,54 +4,59 @@ module Srm630Div2Lev2 where
 import Data.List
 import Control.Arrow
 import qualified Data.Map.Strict as M
+import qualified Data.Set as Set
 ------------------------------------------------------
+frequency :: [(Int,Int)]->[((Int,Int),Int)]
 frequency xs         = map (head &&& length) $ group $ sort xs
+------------------------------------------------------
+-- mergeFrequency :: [((Int,Int),Int)]->[((Int,Int),Int)]->[((Int,Int),Int)]
 mergeFrequency xs ys = M.toList$ M.fromListWith (+) $ xs++ys
 ------------------------------------------------------
 findAllNewPaths ps (a,b,l) =
-       [(a,b,l)]                       -- (a,b)
-    ++ [(a,u,l+d)|(t,u,d)<-ps, t==b]   -- (a,b)++(b,..u)        => (a,u)  
-    ++ [(t,b,d+l)|(t,u,d)<-ps, u==a]   --        (t,..a)++(a,b) => (t,b)
-    ++ [(b,u,l+d)|(t,u,d)<-ps, t==a]   -- (b,a)++(a,..u)        => (b,u)
-    ++ [(t,a,d+l)|(t,u,d)<-ps, u==b]   --        (t,..b)++(b,a) => (t,a)
-    ++ [(t1,u2,d1+l+d2) | (t1,u1,d1)<-ps, (t2,u2,d2)<-ps, u1==a, t2==b]
+       [(a,b,l)]                        -- (a,b             ) => (a,b)
+    ++ [(a,u,l+d)|(t,u,d)<-ps, t==b]    -- (a,b=t,..u       ) => (a,u)
+    ++ [(t,b,d+l)|(t,u,d)<-ps, u==a]    -- (    t,..u=a,b   ) => (t,b)
+    ++ [(b,u,l+d)|(t,u,d)<-ps, t==a]    -- (b,a=t,..u       ) => (b,u)
+    ++ [(t,a,d+l)|(t,u,d)<-ps, u==b]    -- (    t,..u=b,a   ) => (t,a)
+    ++ [(t,u,d1+l+d2) | (t, u1,d1)<-ps, -- (t,..u1=a,b=t2..u) => (t,u)
+                        (t2,u, d2)<-ps, u1==a, t2==b]
 ------------------------------------------------------
-updateOutDistanceCounts outCnts newPaths = newOutCnts
+-- updateOutDistanceCounts ::[((Int,Int),Int)]->[(Int,Int,Int)]->[((Int,Int),Int)]
+updateOutDistanceCounts outCnts newPaths = newOutCnts'
     where
         newOutCnts  = frequency $ [(t,d)| (t,u,d)<-newPaths] ++ [(u,d)| (t,u,d)<-newPaths]
-        newOutCnts' = mergeFrequency outCnts newOutCnts'
+        newOutCnts' = mergeFrequency outCnts newOutCnts
 ------------------------------------------------------
+-- handler :: ([((Int,Int),Int)],[(Int,Int,Int)])->(Int,Int,Int)->([((Int,Int),Int)],[(Int,Int,Int)])
 handler (outCnts, paths) (a,b,l) = (outCnts', paths')
     where
         newPath  = findAllNewPaths paths (a,b,l)
         paths'   = paths ++ newPath
         outCnts' = updateOutDistanceCounts outCnts newPath
 ------------------------------------------------------
-egalitarianism3Easy n | n < 3     = (\_ _ _ ->n)
-                      | otherwise = egalitarianism3Easy'
-egalitarianism3Easy' xs ys lengths =
-    maximum $ map snd $ fst $ foldl handler ([],[]) $ zip3 xs ys lengths
+-- egalitarianism3Easy :: Int->[Int]->[Int]->[Int]->Int
+egalitarianism3Easy n xs ys lengths
+    | n <= 2    = [(1,1,1)] --  n
+    | otherwise = snd $ foldl handler ([],[]) $ zip3 xs ys lengths -- maximum $ map snd $ fst $ foldl handler ([],[]) $ zip3 xs ys lengths
 ------------------------------------------------------
-test  1 xs ys = 0
-test  n xs ys = length $ zip xs ys
 main = do
---    print $ updateCount (1,6) $ M.fromList [((2,3),6), ((1,2),4)]
     print $ egalitarianism3Easy 4  [1,1,1]
                                    [2,3,4]
                                    [1,1,1]
-    print $ egalitarianism3Easy 6  [1,2,3,2,3]
-                                   [2,3,4,5,6]
-                                   [2,1,3,2,3]
+    print $ "-------------------------------------"
+    print $ egalitarianism3Easy 6  [1,2,4,3,3]
+                                   [2,5,3,2,6]
+                                   [2,2,3,1,3]
+    print $ "-------------------------------------"
     print $ egalitarianism3Easy 10 [1,1,1,1,1,1,1,1,1]
                                    [2,3,4,5,6,7,8,9,10]
                                    [1000,1000,1000,1000,1000,1000,1000,1000,1000]
+    print $ "-------------------------------------"
     print $ egalitarianism3Easy 2  [1]
                                    [2]
                                    [3]
---    print $ egalitarianism3Easy 1  [] [] []
+    print $ egalitarianism3Easy 1  [] [] []
 
-    print $ test 1 [] []
-    print $ test 3 [3,4] [6,7]
 
 
 {- Output
