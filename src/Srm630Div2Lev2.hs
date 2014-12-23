5a2ff807a48d6a43aff13628503c1f7e11bd8cac
kts123 <taeksoon@gmail.com>
2014-12-23 09:54:53 -0800

    refactoring

--- src/Srm630Div2Lev2.hs
+++ src/Srm630Div2Lev2.hs
@@ -1,9 +1,10 @@
 -- Srm630Div2Lev2 Egalitarianism3Easy
 -- http://community.topcoder.com/stat?c=problem_statement&pm=13376
 module Srm630Div2Lev2 where
-import Data.List
-import Data.Maybe
-import qualified Data.Map.Strict as M
+import Data.List  -- for nub
+import Data.Maybe -- for Just
+import qualified Data.Map.Strict as M  -- fromList, toList, union
+------------------------------------------------------
 -- cluster :: (distance, nodes [node_0,node_1,..])
 makeCluster  ((a,b),d) = (d, [a,b])
 ------------------------------------------------------
@@ -16,41 +17,41 @@ updateClusters clusters nodes pathTable = clusters'
                   isSame n'    = elem (Just distance) [lookup (n,n'), lookup (n',n)]
                   cluster'     = (distance, nodes ++ (if all isSame nodes then [n] else []))
 ------------------------------------------------------
-findAllNewPaths :: M.Map (Int,Int) Int -> ((Int,Int), Int) -> M.Map (Int,Int) Int 
-findAllNewPaths paths ((a,b),l) = M.fromList $ 
-       [((a,b),l)]                          -- (a,b             ) => (a,b)
-    ++ [((a,u),l+d)|((t,u),d)<-ps, t==b]    -- (a,b=t,..u       ) => (a,u)
-    ++ [((t,b),d+l)|((t,u),d)<-ps, u==a]    -- (    t,..u=a,b   ) => (t,b)
-    ++ [((b,u),l+d)|((t,u),d)<-ps, t==a]    -- (b,a=t,..u       ) => (b,u)
-    ++ [((t,a),d+l)|((t,u),d)<-ps, u==b]    -- (    t,..u=b,a   ) => (t,a)
-    ++ [((t,u),d1+l+d2) |((t, u1),d1)<-ps,  -- (t,..u1=a,b=t2..u) => (t,u)
-                         ((t2,u), d2)<-ps, u1==a, t2==b]
-    where ps = M.toList paths
+findAllNewPaths :: M.Map (Int,Int) Int -> ((Int,Int), Int) -> [((Int,Int), Int)] 
+findAllNewPaths pathTable ((a,b),l) = [((a,b),l)] ++ add (a,b) ++ add (b,a)
+    where add (a, b) = [((a,u),l+d)|((t,u),d)<-ps, t==b]      -- (a,    b=t,..u       ) => (a,u  )
+                    ++ [((t,b),d+l)|((t,u),d)<-ps, u==a]      -- (        t,..u=a,  b ) => (t,b  )
+                    ++ [((t1,u2),d1+l+d2)|((t1, u1), d1)<-ps, -- (t1,..u1=a,  b=t2..u2) => (t1,u2)
+                                          ((t2, u2), d2)<-ps, u1==a && t2==b]
+          ps = M.toList pathTable
 ------------------------------------------------------
 handler :: ([(Int, [Int])], M.Map (Int, Int) Int) -> (Int, Int, Int)-> ([(Int, [Int])], M.Map (Int,Int) Int)
 handler (clusters, pathTable) (a,b,l) = (clusters'', pathTable')
-    where
-        added      = findAllNewPaths pathTable ((a,b),l)
-        pathTable' = M.union pathTable added
-        nodes      = nub $ foldl (\s (a,b)->s++[a,b]) [] $ map fst $ M.toList added 
-        clusters'  = updateClusters clusters nodes pathTable'
-        clusters'' = clusters' ++ (map makeCluster $ M.toList added)
+    where newPaths   = findAllNewPaths pathTable ((a,b),l)
+          pathTable' = M.union pathTable $ M.fromList newPaths
+          nodes      = nub $ foldl (\s ((a,b),l)->s++[a,b]) [] newPaths
+          clusters'  = updateClusters clusters nodes pathTable'
+          clusters'' = clusters' ++ map makeCluster newPaths
 ------------------------------------------------------
 egalitarianism3Easy n xs ys lengths
     | n <= 2    = n
-    | otherwise = maximum    $
-                  map length $
-                  map snd    $
-                  fst $
-                  foldl handler ([], M.empty) $ zip3 xs ys lengths 
+    | otherwise = maximum                     $ --- max size   = maximum [nodesize]
+                  map (length.snd)            $ --- node sizes = [nodesize] = [length [nodes]]
+                  fst                         $ --- clusters   = [cluster]  = [(distance,[nodes])]
+                  foldl handler ([], M.empty) $ --- last state = (clusters, pathTable)
+                  zip3 xs ys lengths            --- pathInfos  = [(a,b,l)]
 ------------------------------------------------------
+double x = x + x
+add3   x = x + 3
 main = do
+--    print $ ^ (a,b)
+    print $ (add3.double)  2
     print $ egalitarianism3Easy 4  [1,1,1]
-                                  [2,3,4]
-                                  [1,1,1]
+                                   [2,3,4]
+                                   [1,1,1]
 
-    print $ egalitarianism3Easy 6  [1,2,4,3,3]
-                                   [2,5,3,2,6]
+    print $ egalitarianism3Easy 6  [1,2,4,2,3]
+                                   [2,5,3,3,6]
                                    [2,2,3,1,3]
     print $ egalitarianism3Easy 10 [1,1,1,1,1,1,1,1,1]
                                    [2,3,4,5,6,7,8,9,10]
