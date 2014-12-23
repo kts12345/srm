50475a8c92ff7a9eb077ed3ab0d770fa343b242a
kts123 <taeksoon@gmail.com>
2014-12-23 01:24:09 -0800

    bug. bug. bug....

--- src/Srm630Div2Lev2.hs
+++ src/Srm630Div2Lev2.hs
@@ -2,68 +2,86 @@
 -- http://community.topcoder.com/stat?c=problem_statement&pm=13376
 module Srm630Div2Lev2 where
 import Data.List
+import Data.Maybe
 import Control.Arrow
 import qualified Data.Map.Strict as M
-import qualified Data.Set as S
 ------------------------------------------------------
-addToCluster []          (t,u,d) = [(t,u,d)]
-addToCluster (e:cluster) (t,u,d) = if ()
+allM f xs = all isJust xs  
+            && 
+            all (\e-> f (fromJust e)) xs 
 ------------------------------------------------------
-frequency :: [(Int,Int)]->[((Int,Int),Int)]
-frequency xs         = map (head &&& length) $ group $ sort xs
+-- cluster :: (distance, set [node_0,node_1,..]) 
+makeCluster :: ((Int,Int),Int)->(Int,[Int])
+makeCluster  ((a,b),d) = (d, [a,b])
 ------------------------------------------------------
--- mergeFrequency :: [((Int,Int),Int)]->[((Int,Int),Int)]->[((Int,Int),Int)]
-mergeFrequency xs ys = M.toList$ M.fromListWith (+) $ xs++ys
+lookDistance :: Int -> Int -> M.Map (Int,Int) Int -> Maybe Int 
+lookDistance a b pathTable 
+  | isJust (distance a b) = distance a b 
+  | otherwise             = distance b a 
+      where distance t u    = M.lookup (b,a) pathTable
 ------------------------------------------------------
-findAllNewPaths ps (a,b,l) =
-       [(a,b,l)]                        -- (a,b             ) => (a,b)
-    ++ [(a,u,l+d)|(t,u,d)<-ps, t==b]    -- (a,b=t,..u       ) => (a,u)
-    ++ [(t,b,d+l)|(t,u,d)<-ps, u==a]    -- (    t,..u=a,b   ) => (t,b)
-    ++ [(b,u,l+d)|(t,u,d)<-ps, t==a]    -- (b,a=t,..u       ) => (b,u)
-    ++ [(t,a,d+l)|(t,u,d)<-ps, u==b]    -- (    t,..u=b,a   ) => (t,a)
-    ++ [(t,u,d1+l+d2) | (t, u1,d1)<-ps, -- (t,..u1=a,b=t2..u) => (t,u)
-                        (t2,u, d2)<-ps, u1==a, t2==b]
+addToCluster :: (Int,[Int]) -> Int -> M.Map (Int,Int) Int -> (Int, [Int])
+addToCluster (distance, nodes) node  pathTable = (distance, nodes') 
+  where
+    findDistance node' = lookDistance node node' pathTable 
+    allD     = allM  (== distance) $ map findDistance nodes 
+    nodes'   = nodes ++ (if allD then [node] else [])
 ------------------------------------------------------
--- updateOutDistanceCounts ::[((Int,Int),Int)]->[(Int,Int,Int)]->[((Int,Int),Int)]
-updateOutDistanceCounts outCnts newPaths = newOutCnts'
+updateClusters :: [(Int, [Int])] -> [Int] -> M.Map (Int,Int) Int -> [(Int, [Int])] 
+updateClusters clusters nodes pathTable = clusters' 
+  where add :: Int->(Int,[Int])->(Int,[Int])
+        add node cluster = addToCluster cluster node pathTable 
+        handler :: [(Int, [Int])]->Int->[(Int, [Int])]
+        handler clusters a = map (add a) clusters
+        clusters' = foldl handler clusters nodes
+------------------------------------------------------
+findAllNewPaths :: M.Map (Int,Int) Int -> ((Int,Int), Int) -> M.Map (Int,Int) Int 
+findAllNewPaths paths ((a,b),l) = M.fromList $ 
+       [((a,b),l)]                          -- (a,b             ) => (a,b)
+    ++ [((a,u),l+d)|((t,u),d)<-ps, t==b]    -- (a,b=t,..u       ) => (a,u)
+    ++ [((t,b),d+l)|((t,u),d)<-ps, u==a]    -- (    t,..u=a,b   ) => (t,b)
+    ++ [((b,u),l+d)|((t,u),d)<-ps, t==a]    -- (b,a=t,..u       ) => (b,u)
+    ++ [((t,a),d+l)|((t,u),d)<-ps, u==b]    -- (    t,..u=b,a   ) => (t,a)
+    ++ [((t,u),d1+l+d2) |((t, u1),d1)<-ps,  -- (t,..u1=a,b=t2..u) => (t,u)
+                         ((t2,u), d2)<-ps, u1==a, t2==b]
+    where ps = M.toList paths
+------------------------------------------------------
+handler :: ([(Int, [Int])], M.Map (Int, Int) Int) -> (Int, Int, Int)-> ([(Int, [Int])], M.Map (Int,Int) Int)
+handler (clusters, pathTable) (a,b,l) = (clusters'', pathTable')
     where
-        newOutCnts  = frequency $ [(t,d)| (t,u,d)<-newPaths] ++ [(u,d)| (t,u,d)<-newPaths]
-        newOutCnts' = mergeFrequency outCnts newOutCnts
-------------------------------------------------------
--- handler :: ([((Int,Int),Int)],[(Int,Int,Int)])->(Int,Int,Int)->([((Int,Int),Int)],[(Int,Int,Int)])
-handler (outCnts, paths) (a,b,l) = (outCnts', paths')
-    where
-        newPath  = findAllNewPaths paths (a,b,l)
-        paths'   = paths ++ newPath
-        outCnts' = updateOutDistanceCounts outCnts newPath
+        added      = findAllNewPaths pathTable ((a,b),l)
+        pathTable' = M.union pathTable added 
+        nodes      = foldl (\s (a,b)->s++[a,b]) [] $ map fst $ M.toList added 
+        clusters'  = updateClusters clusters nodes pathTable' 
+        clusters'' = clusters' ++ [(l,[a,b])] 
 ------------------------------------------------------
 -- egalitarianism3Easy :: Int->[Int]->[Int]->[Int]->Int
 egalitarianism3Easy n xs ys lengths
-    | n <= 2    = [(1,1,1)] --  n
-    | otherwise = snd $ foldl handler ([],[]) $ zip3 xs ys lengths -- maximum $ map snd $ fst $ foldl handler ([],[]) $ zip3 xs ys lengths
+    | n <= 2    = n
+    | otherwise = maximum    $
+                  map length $
+                  map snd    $
+                  fst $ 
+                  foldl handler ([], M.empty) $ zip3 xs ys lengths 
 ------------------------------------------------------
-main = do
-    print $ S.Set[(2,3)]
+m = M.fromList[((2,3),4), ((1,2),4)]
+main = do 
     print $ egalitarianism3Easy 4  [1,1,1]
                                    [2,3,4]
                                    [1,1,1]
-    print $ "-------------------------------------"
     print $ egalitarianism3Easy 6  [1,2,4,3,3]
                                    [2,5,3,2,6]
                                    [2,2,3,1,3]
-    print $ "-------------------------------------"
     print $ egalitarianism3Easy 10 [1,1,1,1,1,1,1,1,1]
                                    [2,3,4,5,6,7,8,9,10]
                                    [1000,1000,1000,1000,1000,1000,1000,1000,1000]
-    print $ "-------------------------------------"
     print $ egalitarianism3Easy 2  [1]
                                    [2]
                                    [3]
     print $ egalitarianism3Easy 1  [] [] []
 
 
-
-{- Output
+{- Expected Output
 3
 3
 9
