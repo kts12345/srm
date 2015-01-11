1726dddac593dcbedc2b689386a4f118b6e66e39
kts123 <taeksoon@gmail.com>
2015-01-11 08:29:11 -0800

    indentation for pretty look

--- src/Srm628Div2Lev1.hs
+++ src/Srm628Div2Lev1.hs
@@ -3,11 +3,12 @@
 module Srm628Div2Lev1 where
 ------------------------------------------------------
 bishopMove r1 c1 r2 c2
-  | odd (x+y)      = -1
-  | (x,y) == (0,0) =  0
-  | abs x == abs y =  1
-  | otherwise      =  2
-      where (x,y)  = (r2-r1, c2-c1)
+  |  odd (x+y)        = -1
+  |  (x,y) == (0,0)   =  0
+  |  abs x == abs y   =  1
+  |  otherwise        =  2
+  where
+      (x,y) = (r2-r1, c2-c1)
 ------------------------------------------------------
 main = do
  print $ bishopMove  4 6 7 3
