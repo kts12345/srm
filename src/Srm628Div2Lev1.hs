4d0a614608cc9a5a559b3e1c61a3e05d3a7ade4e
kts123 <taeksoon@gmail.com>
2015-01-11 19:56:10 -0800

    explicit function - distinct pairty

--- src/Srm628Div2Lev1.hs
+++ src/Srm628Div2Lev1.hs
@@ -2,13 +2,15 @@
 -- http://community.topcoder.com/stat?c=problem_statement&pm=13280
 module Srm628Div2Lev1 where
 ------------------------------------------------------
+distinctParity x y  =  odd (x + y)
+------------------------------------------------------
 bishopMove r1 c1 r2 c2
-  |  odd (x+y)        = -1
-  |  (x,y) == (0,0)   =  0
-  |  abs x == abs y   =  1
-  |  otherwise        =  2
+  |  distinctParity x y  = -1   -- distinct parity
+  |  (x,y) == (0,0)      =  0   -- same     parity - origin
+  |  abs x == abs y      =  1   -- same     parity - same absolute value
+  |  otherwise           =  2   -- same     parity - otherwise
   where
-      (x,y) = (r2-r1, c2-c1)
+      (x,y) = (r2-r1, c2-c1)    -- normalize
 ------------------------------------------------------
 main = do
  print $ bishopMove  4 6 7 3
