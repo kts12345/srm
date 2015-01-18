20437f923f7f59a2eef4531fb73d00f654c7725b
kts123 <taeksoon@gmail.com>
2015-01-18 09:40:24 -0800

    Srm627Div2Lev1

---
+++ src/Srm627Div2Lev1.hs
@@ -0,0 +1,35 @@
+-- Srm627Div2Lev1 ManySquare
+-- http://community.topcoder.com/stat?c=problem_statement&pm=13277&rd=16008
+module Srm627Div2Lev1 where
+------------------------------------------------------
+import qualified Data.Map as M
+------------------------------------------------------
+handler (_,table) stick
+    | cnt == 3   = (1, M.delete stick table)
+    | otherwise  = (0, M.insert stick (cnt+1) table)
+      where cnt  =  M.findWithDefault 0 stick table
+------------------------------------------------------
+manySquares xs = last           $ -- 4.                                                                 1
+    scanl (+) 0                 $ -- 3.[ 0       ,  0       ,  0           ,  0,             1       ,  1        ]
+    map   fst                   $ -- 2.[ 0       ,  0       ,  0           ,  0,          ,  1       ,  0        ]
+    scanl handler (0, M.empty)  $ -- 1.[(0,"1:1"), (0,"1:2"), (0,"1:2,5:1"), (0,"1:3,5:1"), (1,"5:1"), (0, "5:2")]
+    xs                            -- 0.[  1     ,    1    ,     5          ,   1          ,   1       ,  5       ]
+------------------------------------------------------
+main = do    
+    print $ manySquares  [1,1,2,2,1,1,2]
+    print $ manySquares  [3,1,4,4,4,10,10,10,10]
+    print $ manySquares  [1,2,3,4,1,2,3,4,1,2,3,1,2,3,4,1,2,3,3,3]
+    print $ manySquares  [1,1,1,2,2,2,3,3,3,4,4,4]
+    print $ manySquares  [1,1,1,2,1,1,1,3,1,1,1]
+    print $ manySquares  [2,2,4,4,8,8]
+
+{- Output
+ 1
+ 1
+ 3
+ 0
+ 2
+ 0
+-}
+------------------------------------------------------
+-- last.scanl (+) 0  == sum
