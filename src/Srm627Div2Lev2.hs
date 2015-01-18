c6e799542b843f0e6bd447190937aa53690eaba8
kts123 <taeksoon@gmail.com>
2015-01-18 10:35:22 -0800

    refactoring

--- src/Srm627Div2Lev2.hs
+++ src/Srm627Div2Lev2.hs
@@ -4,15 +4,17 @@ module Srm627Div2Lev2 where
 ------------------------------------------------------
 import qualified Data.Map as M
 ------------------------------------------------------
-handler (maxLetter, maxCnt, table, totalCnt) letter = (maxLetter', maxCnt', table', totalCnt+1)
+handler (maxLetter, maxCnt, table, len) letter = (maxLetter', maxCnt', table', len+1)
     where cnt'   = 1 + M.findWithDefault 0 letter table
           table' = M.insert letter cnt' table
-          (maxLetter', maxCnt') = if maxCnt < cnt' then (letter, cnt') else (maxLetter, maxCnt)
-          
+          (maxLetter', maxCnt') | maxCnt < cnt'  = (letter   , cnt'  )
+                                | otherwise      = (maxLetter, maxCnt)
 ------------------------------------------------------
-happyLetter xs = last $  
-    map (\(a,b,_,d)-> if (2*b > d) then a else '.') $ 
-    scanl handler ('a', 0, M.empty, 0) $ 
+calcHappy letter cnt len = if (2*cnt) > len then letter else '.'
+------------------------------------------------------
+happyLetter xs = last                    $
+    map (\(a,b,_,d)-> calcHappy a b d)   $
+    scanl handler ('a', 0, M.empty, 0)   $
     xs
 ------------------------------------------------------
 main = do 
