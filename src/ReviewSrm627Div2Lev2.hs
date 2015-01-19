9b8f22f6061d7440771d397bfd855e4b7fa8b643
kts123 <taeksoon@gmail.com>
2015-01-18 23:23:59 -0800

    ReviewSrm627Div2Lev2

--- src/ReviewSrm627Div2Lev2.hs
+++ src/ReviewSrm627Div2Lev2.hs
@@ -5,9 +5,7 @@ import Data.List
 import Data.Tuple
 import qualified Data.Map as M
 
-updateMap m l = M.insertWith (+) l 1 m
-
-updateState (count, m) c = (count + 1, updateMap m c)
+updateState m c = M.insertWith (+) c 1 m
 
 compareSwap v1 v2 = compare (swap v1) (swap v2)
 
@@ -16,16 +14,16 @@ toHappyLetter (total, (c, count)) | div total 2 < count = c
 
 getHappyLetter cs = 
           last
-        $ map toHappyLetter
-        $ map (\(count, ls) -> (count, maximumBy compareSwap ls))
-        $ map (\(count, m)  -> (count, M.toList m))
-        $ tail
-        $ scanl updateState (0, M.empty) cs
+        $ map toHappyLetter 
+        $ zip [0,1..]
+        $ map (\ls -> maximumBy compareSwap ls)
+        $ map (\m  -> M.toList m)
+        $ scanl updateState M.empty cs
 
 main = do
     print $ getHappyLetter "aacaa"
     print $ getHappyLetter "dcdjx"
     print $ getHappyLetter "bcbbbbba"
     print $ getHappyLetter "aabc"
-    print $ getHappyLetter "oyyyoowoooofwjoooooffyyooo"         -- o    
+    print $ getHappyLetter "oyyyoowoooofwjoooooffyyooo"         -- o
     print $ getHappyLetter "jyjjkjjkjjykkkkyjyjkkkyjkykyjjkkjkjykkxjjk"
