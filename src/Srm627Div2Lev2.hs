aaa7b50cee7b35cca845260a7a1235ce20027c36
kts123 <taeksoon@gmail.com>
2015-01-20 07:30:59 -0800

    pretty look ?

--- src/Srm627Div2Lev2.hs
+++ src/Srm627Div2Lev2.hs
@@ -1,14 +1,13 @@
--- Srm627Div2Lev2 HappyLetter
 -- http://community.topcoder.com/stat?c=problem_statement&pm=13245&rd=16008
 module Srm627Div2Lev2 where
 ------------------------------------------------------
 import qualified Data.Map as M
 import Data.Maybe
 ------------------------------------------------------
-handler (tbl, (numMx, chMx)) ch = (tbl', max (numMx, chMx) (num, ch))
-    where (old, tbl') = M.insertLookupWithKey (\_ _ o -> o+1) ch 1 tbl
-          num | isJust old = 1 + fromJust old
-              | otherwise  = 1
+handler (tbl, (numMx, chMx)) ch = (tbl', max (numMx, chMx) (num, ch)) where 
+    (old, tbl') = M.insertLookupWithKey (\_ _ o -> o+1) ch 1 tbl
+    num | isJust old = 1 + fromJust old
+        | otherwise  = 1
 ------------------------------------------------------
 toHappy total num ch | total < (2*num) = ch
                      | otherwise       = '.'
@@ -18,7 +17,7 @@ happyLetter xs =
     map   toHappy'                   $
     zip   [0,1..]                    $
     scanl handler (M.empty, (0,'a')) $
-    xs
+    xs 
     where toHappy' (total, (_, (num, ch))) = toHappy total num ch
 ------------------------------------------------------
 main = do 
